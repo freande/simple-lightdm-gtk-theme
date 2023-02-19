d182e2081a8564a25a43f2e6257ae95fdf9af980
freande <75705976+freande@users.noreply.github.com>
2023-02-19 18:56:34 +0100

    Fiix install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo cp lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
-sudo cp -r simple-lightdm /usr/share/themes/simple-lightdm
+sudo cp -r simple-lightdm /usr/share/themes/
