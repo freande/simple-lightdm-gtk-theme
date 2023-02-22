74d245ccf4a19f6489e39f78168f5f5f19a1dfcb
freande <75705976+freande@users.noreply.github.com>
2023-02-22 19:20:37 +0100

    Fix current dir in path for installation

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-sudo cp lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
-sudo cp -r simple-lightdm /usr/share/themes/
+CUR_DIR="$(dirname "$0")"
+
+sudo cp $CUR_DIR/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
+sudo cp -r $CUR_DIR/simple-lightdm /usr/share/themes/
